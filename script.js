63708d2d514fc551611b2fb6858787e44dfe5c70
amschajari <a.m.saposnik@gmail.com>
2025-02-06 19:49:04 -0300

    Implementado panel de capas colapsado con OpenStreetMap y Satélite

--- script.js
+++ script.js
@@ -8,9 +8,6 @@ const baseLayers = {
   }),
   "Satélite": L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=TU_TOKEN_MAPBOX', {
     attribution: '© Mapbox'
-  }),
-  "Terreno": L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenTopoMap contributors'
   })
 };
 
@@ -22,30 +19,7 @@ L.marker([-34.6037, -58.3816]).addTo(map)
   .bindPopup('¡Hola! Este es un marcador en Buenos Aires.')
   .openPopup();
 
-// Cambiar el mapa base al hacer clic en los botones
-document.getElementById('osm-button').addEventListener('click', () => {
-  map.eachLayer(layer => {
-    if (layer instanceof L.TileLayer) {
-      map.removeLayer(layer);
-    }
-  });
-  baseLayers["OpenStreetMap"].addTo(map);
-});
-
-document.getElementById('satellite-button').addEventListener('click', () => {
-  map.eachLayer(layer => {
-    if (layer instanceof L.TileLayer) {
-      map.removeLayer(layer);
-    }
-  });
-  baseLayers["Satélite"].addTo(map);
-});
-
-document.getElementById('terrain-button').addEventListener('click', () => {
-  map.eachLayer(layer => {
-    if (layer instanceof L.TileLayer) {
-      map.removeLayer(layer);
-    }
-  });
-  baseLayers["Terreno"].addTo(map);
-});
+// Crear el control de capas
+const layerControl = L.control.layers(baseLayers, null, {
+  collapsed: true // Panel colapsado
+}).addTo(map);
